3532375579213450b6ea2d52c31f1f6fa7ba4237
Fernand Lime <fernand.lime@gmail.com>
2024-10-18 16:55:32 +0200

    Initial Commit

---
+++ cityquest/cityquestApp.swift
@@ -0,0 +1,17 @@
+//
+//  cityquestApp.swift
+//  cityquest
+//
+//  Created by Fernand LIME on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct cityquestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
