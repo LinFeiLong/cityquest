aac1f21d09f81cbab4a0c9c637b2d7d67139c3be
Fernand LIME <fernand.lime@gmail.com>
2024-10-24 12:16:38 +0200

    Add Buttons + Colors

---
+++ cityquest/Components/ButtonView.swift
@@ -0,0 +1,45 @@
+//
+//  ButtonView.swift
+//  cityquest
+//
+//  Created by Fernand LIME on 24/10/2024.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+
+    var label: String
+    var icon: String
+    var fontColor: Color
+    var color: Color
+    var action: () -> Void
+
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .resizable()
+                .frame(width: 20, height: 20)
+            Text(label)
+                .fontWeight(.bold)
+        }
+        .foregroundStyle(fontColor)
+        .fontDesign(.default)
+        .padding()
+        .frame(maxWidth: .infinity) // Make the button full width
+        .background(color)
+        .clipShape(
+            RoundedRectangle(cornerSize: CGSize(width: 40, height: 40))
+        )
+    }
+}
+
+#Preview {
+    VStack {
+        ButtonView(label: "Jouer", icon: "play.fill", fontColor: Color("MainColor"), color: Color("AccentColor")) {
+            print("Button tapped!")
+        }
+    }
+    .padding() // Optional: Add some padding around the VStack
+}
+
