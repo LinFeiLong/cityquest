641bd811586e65d386a94550c79f3223283c1697
BARTOSNicolas <73882719+BARTOSNicolas@users.noreply.github.com>
2024-10-24 14:55:28 +0200

    GameModel

---
+++ cityquest/Game/GameMapView.swift
@@ -0,0 +1,18 @@
+//
+//  GameMapView.swift
+//  cityquest
+//
+//  Created by Bartos Nicolas on 24/10/2024.
+//
+
+import SwiftUI
+
+struct GameMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameMapView()
+}
