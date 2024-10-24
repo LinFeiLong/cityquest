61354d462c3b1a6b53366c4630c82de359f6b9ef
Ezequiel Gomes <ezequiel7897@gmail.com>
2024-10-24 17:15:33 +0200

    Start Of DescriptionView

---
+++ cityquest/DescriptionView.swift
@@ -0,0 +1,40 @@
+//
+//  DescriptionView.swift
+//  cityquest
+//
+//  Created by Ezequiel Gomes on 24/10/2024.
+//
+
+import SwiftUI
+
+struct DescriptionView: View {
+    let monuments = Monument.seed
+
+    var body: some View {
+        ZStack {
+            Color.main.ignoresSafeArea()
+            ScrollView{
+
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("Les monuments phares :").font(.title2)
+                        GridCardPlacesComponent(places: monuments)
+                        Text("Les monuments phares :").font(.title2)
+                        GridCardPlacesComponent(places: monuments)
+                    }
+
+                    ButtonView(label: "Jouer", icon: "play.fill", fontColor: .main, color: .accent) {}.padding(.vertical)
+                }.padding()
+
+
+            }.navigationTitle("Titre de la ville").navigationBarTitleDisplayMode(.automatic)
+        }
+
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DescriptionView()
+    }
+}
