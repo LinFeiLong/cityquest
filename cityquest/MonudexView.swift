d388b587640bf38cd946ce743ee527f5a945d67a
Ezequiel Gomes <ezequiel7897@gmail.com>
2024-10-24 16:14:36 +0200

    Add of Monudex View + GridCardPlacesComponent

---
+++ cityquest/MonudexView.swift
@@ -0,0 +1,32 @@
+//
+//  MonudexView.swift
+//  cityquest
+//
+//  Created by Ezequiel Gomes on 24/10/2024.
+//
+
+import SwiftUI
+
+struct MonudexView: View {
+    let title: String = "Marseille"
+    let monuments : [Monument]
+    var body: some View {
+
+        NavigationStack {
+            ZStack {
+                Color.main.edgesIgnoringSafeArea(.all)
+                VStack() {
+                    Text(title).font(.title2).foregroundStyle(.accent).fontWeight(.semibold).padding(.vertical)
+                    GridCardPlacesComponent(places: monuments)
+
+                    Spacer()
+                }.navigationTitle("Mon Monudex")
+            }
+        }
+
+    }
+}
+#Preview {
+    MonudexView(monuments: Monument.seed)
+}
+
