aa5296d0311a03360f31d3fd1e60ea3bf2ece1ec
Fernand LIME <fernand.lime@gmail.com>
2024-10-28 14:08:35 +0100

    Add background view

---
+++ cityquest/Components/QuestionBG.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionBG.swift
+//  cityquest
+//
+//  Created by Fernand LIME on 28/10/2024.
+//
+
+import SwiftUI
+
+struct QuestionBG: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color("MainColor"))
+            .cornerRadius(25)
+            .padding()
+    }
+}
+
+#Preview {
+    QuestionBG()
+}
