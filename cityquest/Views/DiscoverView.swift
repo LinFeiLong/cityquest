de6a2c698bd1a015cf8b101e0400d890b784e000
Anthony <pro.anthony.pandolfi@icloud.com>
2024-10-31 14:32:06 +0100

    Image String Modification

--- cityquest/Views/DiscoverView.swift
+++ cityquest/Views/DiscoverView.swift
@@ -55,19 +55,19 @@ func testPlay() {
                     name: "Paris",
                     description: "The city of lights",
                     coordinate: Coordinate(latitude: 48.8566, longitude: 2.3522),
-                    image: .imageTestMonument
+                    image: ".imageTestMonument"
                 ),
                 City(
                     name: "Marseille",
                     description: "The port city",
                     coordinate: Coordinate(latitude: 43.2965, longitude: 5.3698),
-                    image: .imageTestMonument
+                    image: ".imageTestMonument"
                 ),
                 City(
                     name: "Lyon",
                     description: "The gastronomic capital",
                     coordinate: Coordinate(latitude: 45.7640, longitude: 4.8357),
-                    image: .imageTestMonument
+                    image: ".imageTestMonument"
                 )
             ]
         )
