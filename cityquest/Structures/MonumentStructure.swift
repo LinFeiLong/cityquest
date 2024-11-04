02b6c0bc323bd1bb170c9c14894c963f55b34bc4
Ezequiel Gomes <ezequiel7897@gmail.com>
2024-11-04 15:57:47 +0100

    Ajout Monudex Compatible avec le JSON

--- cityquest/Structures/MonumentStructure.swift
+++ cityquest/Structures/MonumentStructure.swift
@@ -8,12 +8,12 @@
 import Foundation
 import SwiftUI
 
-struct Response: Decodable {
+struct Response: Codable {
     var response: String
     var isCorrect: Bool
 }
 
-struct Question: Decodable {
+struct Question: Codable {
     var question: String
     var isAnsweredCorrectly: Bool
     var isAnswered: Bool
@@ -35,21 +35,16 @@ class Monument: Place {
         )
     }
     
-    required init(from decoder: Decoder) throws {
+    required init(from decoder: any Decoder) throws {
+
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        // Decode properties
-        let name = try container.decode(String.self, forKey: .name)
-        let description = try container.decode(String.self, forKey: .description)
-        let image = try container.decode(String.self, forKey: .image)
-        let latitude = try container.decode(Double.self, forKey: .latitude)
-        let longitude = try container.decode(Double.self, forKey: .longitude)
         self.questions = try container.decode([Question].self, forKey: .questions)
         let wikipedia_page_url = try container.decode(String.self, forKey: .wikipedia_page_url)
 
         // Call the superclass initializer
         super.init(name: name, description: description, image: image, latitude: latitude, longitude: longitude, wikipedia_page_url: wikipedia_page_url)
     }
+
     
     private enum CodingKeys: String, CodingKey {
         case name, description, image, latitude, longitude, questions, wikipedia_page_url
